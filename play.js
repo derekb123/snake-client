dc15f8a4ae1cc9e4a79c3a590864ea712fc0f134
derekb123 <derekbutvin@gmail.com>
2020-07-29 23:56:56 +0000

    separated out input file

--- play.js
+++ play.js
@@ -1,31 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  // stdin.on('data', (key) => {
-  //   handleUserInput(key);
-  // });
-
-  stdin.on('data', handleUserInput);
-
-
-  return stdin;
-}
-
+setupInput();
